7ebeb35b19b879ab39f41ac42a76c7affb29d991
humechanic <j4nuary@mail.ru>
2020-11-18 12:28:25 +0300

    TASK-3: add basic architecture, add redux

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./../sass/main.module.scss";
+import { requestAllPokemons } from "./../model/requestConstructor";
+
+const MainPage = () => {
+    // const checkingDataStructureIget = requestAllPokemons();
+    return (
+        <>
+            <div className={classes.contentWrapper}>privet</div>
+        </>
+    );
+};
+
+export default MainPage;
