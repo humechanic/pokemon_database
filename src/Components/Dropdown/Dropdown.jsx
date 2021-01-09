3fc139b2b1738e084d529b01b2b8f2ce73064195
humechanic <j4nuary@mail.ru>
2021-01-09 11:09:25 +0300

    WIP TASK-9: add search bar and filter by types

---
+++ src/Components/Dropdown/Dropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dropdown = () => {
+    return (
+        <select>
+            <option value="1">6</option>
+            <option value="2">5</option>
+            <option value="3">4</option>
+        </select>
+    )
+}
+
+export default Dropdown;
